b2eddf1be2d17809378cf51bf779cb7fc5793102
Kajin <kajin88@gmail.com>
2019-08-20 13:43:37 +0530

    UnCommented the movie names

--- TestAutothon/TestAutothon/Program.cs
+++ TestAutothon/TestAutothon/Program.cs
@@ -16,23 +16,23 @@ namespace TestAutothon_Combined
             "The Shawshank Redemption",
             "NonExisting",
             "The Godfather",
-            //"The Dark Knight",
-            //"Pulp Fiction",
-            //"Schindler's List",
-            //"The Lord of the Rings: The Return of the King",//* multiple imdb links in wiki
-            //"The Good,The Bad,The Ugly", //* Invalid wiki link in bing
-            //"12 Angry Men",
-            //"Inception",
-            //"Forrest Gump",
-            //"Fight Club",
-            //"Star Wars:Episode V-The Empire Strikes Back", //Works, but confused with video game
-            //"Goodfellas",
-            //"The Matrix", //The Wachowskis(Combined brother names in wiki)
-            //"One Flew Over The Cuckoo's Nest", //Novel vs Movie wiki page and special characters in director name
-            //"Seven Samurai",
-            //"Avengers: Infinity War", // "Anthony Russo\r\nJoe Russo" as single name in wiki
-            //"Interstellar",
-            //"Se7en"
+            "The Dark Knight",
+            "Pulp Fiction",
+            "Schindler's List",
+            "The Lord of the Rings: The Return of the King",//* multiple imdb links in wiki
+            "The Good,The Bad,The Ugly", //* Invalid wiki link in bing
+            "12 Angry Men",
+            "Inception",
+            "Forrest Gump",
+            "Fight Club",
+            "Star Wars:Episode V-The Empire Strikes Back", //Works, but confused with video game
+            "Goodfellas",
+            "The Matrix", //The Wachowskis(Combined brother names in wiki)
+            "One Flew Over The Cuckoo's Nest", //Novel vs Movie wiki page and special characters in director name
+            "Seven Samurai",
+            "Avengers: Infinity War", // "Anthony Russo\r\nJoe Russo" as single name in wiki
+            "Interstellar",
+            "Se7en"
         };
 
         public static AutomationBrowserType browserType;
